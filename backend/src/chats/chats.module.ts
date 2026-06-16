7e4e22032301f3a50a4e47165054a5f09fb22c2a
itsyoni <yonagona@gmail.com>
2026-06-16 21:10:20 +0300

    Add socket.io

--- backend/src/chats/chats.module.ts
+++ backend/src/chats/chats.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 import { ChatsController } from './chats.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ChatsGateway } from './chats.gateway';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ChatsController],
-  providers: [ChatsService],
+  providers: [ChatsService, ChatsGateway],
 })
 export class ChatsModule {}
