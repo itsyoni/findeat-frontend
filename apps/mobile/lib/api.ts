03772d76cdca16cdff64251b211e6d6cd5ec8ebc
itsyoni <yonagona@gmail.com>
2026-07-03 20:05:56 +0300

    added web

--- apps/mobile/lib/api.ts
+++ apps/mobile/lib/api.ts
@@ -5,7 +5,8 @@ import axios from "axios";
 // export const API_URL = "http://172.20.10.6:3000";
 // export const API_URL = "http://172.20.10.3:3000";
 // export const API_URL = "http://10.0.0.16:3000";
-export const API_URL = "http://10.100.10.65:3000";
+// export const API_URL = "http://10.100.10.65:3000";
+export const API_URL = "http://192.168.1.138:3000";
 // export const API_URL = "http://localhost:3000";
 
 export const api = axios.create({
